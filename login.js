5d1923ac81222293e6994e7631847c1ebc146a39
Foxyman26011 <ensomosean@gmail.com>
2025-04-15 23:32:59 +0800

    Update login.js

--- login.js
+++ login.js
@@ -25,35 +25,3 @@ function login() {
   loginScreen.style.display = 'none';
   galleryScreen.style.display = 'block';
 }
-
-function addImage() {
-  const imageInput = document.getElementById('imageInput');
-  const caption = document.getElementById('imageCaption').value;
-  const gallery = document.getElementById('gallery');
-
-  const file = imageInput.files[0];
-  if (!file) {
-    alert("Please select an image.");
-    return;
-  }
-
-  const reader = new FileReader();
-  reader.onload = function (e) {
-    const imgBox = document.createElement('div');
-    imgBox.classList.add('image-box');
-
-    const img = document.createElement('img');
-    img.src = e.target.result;
-
-    const captionEl = document.createElement('p');
-    captionEl.textContent = caption;
-
-    imgBox.appendChild(img);
-    imgBox.appendChild(captionEl);
-    gallery.prepend(imgBox);
-  };
-
-  reader.readAsDataURL(file);
-  imageInput.value = '';
-  document.getElementById('imageCaption').value = '';
-}
